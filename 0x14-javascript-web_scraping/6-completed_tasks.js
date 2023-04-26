75e07b83bfaf6716ec94ff493204f5afa42ee29f
Auwal <auwallinus@gmail.com>
2023-04-25 17:33:49 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,23 @@
 #!/usr/bin/node
+// web scrapper
+
 const request = require('request');
-const args = process.argv;
-const requestURL = args[2];
-const list = {};
-request.get(requestURL, (err, res, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    const data = JSON.parse(body);
-    let user = 'default';
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].completed === true) {
-        if (!(data[i].userId in list)) {
-          user = data[i].userId;
-          list[user] = 1;
-        } else {
-          user = data[i].userId;
-          list[user] += 1;
-        }
+
+request.get(process.argv[2], { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
       }
     }
-    console.log(list);
-  }
+  });
+  console.log(tasksCompleted);
 });
