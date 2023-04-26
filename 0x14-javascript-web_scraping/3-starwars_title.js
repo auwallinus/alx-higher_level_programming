30450112db5b3662272d6880fa8c7678e6a50759
Auwal <auwallinus@gmail.com>
2023-04-25 17:07:41 -0700

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const args = process.argv;
-const requestURL = 'https://swapi-api.hbtn.io/api/films/' + args[2];
+const requestURL = 'https://swapi-api.alx-tools.com/api/films/:id' + args[2];
 
 request.get(requestURL, (err, res, body) => {
   if (err) {
