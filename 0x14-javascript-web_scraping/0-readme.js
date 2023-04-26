8dd3470f876c565d90fd0e1b24b4715ae1ff8b10
Auwal <auwallinus@gmail.com>
2023-04-25 17:29:39 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+// Read from file
 
-const fs = require('fs');
-const args = process.argv;
-
-fs.readFile(args[2], 'utf-8', (err, data) => {
-  if (err) { console.log(err); } else {
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
     console.log(data);
-  }
-});
+  });
